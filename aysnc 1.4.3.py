9c79ae7f2189ef620d88bf56bc8ff4c40ff2570f
Matt Ostendorf <Matt.Ostendorf@du.edu>
2023-09-11 15:30:21 -0700

    updated documentation

--- aysnc 1.4.3.py
+++ aysnc 1.4.3.py
@@ -1,4 +1,13 @@
 def square(n, power):
+    """Calculates power for number upto n
+
+    Args:
+        n (_type_): _description_
+        power (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return [i **power for i in range(n)]
 
 print(square(10,4))
